1f0eb2e2665eacaa2b8b36e87301fa1b58ca40c2
blitzlord <benjamin.jurkowski@gmail.com>
2017-11-21 20:40:24 +0100

    Text2Speech

--- start.js
+++ start.js
@@ -1,26 +1,28 @@
 const port = 8110;
 
-
+const say = require('say');
 var express = require('express');
 var app = express();
 var request = require('request');
 var player = require('play-sound')(opts = {})
 
 // Forward all requests from /api to http://foo.com/api
-app.use('/audio/blocker', function(req, res) {
-    player.play('audio/coin-sound.mp3', function(err){
-      if (err) throw err;
-  });
+app.use('/coin', function(req, res) {
+	player.play('audio/coin-sound.mp3', function(err) {
+		if (err) throw err;
+	});
+});
+app.use('/text/:text', function(req, res) {
+	say.speak(req.params.text);
+	res.send('<h2>' + req.params.text + '</h2>');
 });
 
-
-app.get('/log', function(req, res) {
-
-});
 
 
 
 
 app.listen(port, function() {
-  console.log('\x1b[36m%s\x1b[0m', 'http://localhost:'+port); //cyan
+	console.log('\x1b[36m%s\x1b[0m', 'http://localhost:' + port); //cyan
+	console.log('\x1b[36m%s\x1b[0m', 'http://localhost:' + port + "/text/"); //cyan
+	console.log('\x1b[36m%s\x1b[0m', 'http://localhost:' + port + "/coin"); //cyan
 });
